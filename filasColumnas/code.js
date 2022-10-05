c0cc02318218873651510c51493d0ffecdd3da1b
Your NameYour <kevin.guevara090@gmail.com>
2022-10-05 12:21:11 -0500

    creacion automatica de filas y columnas

--- filasColumnas/code.js
+++ filasColumnas/code.js
@@ -1,5 +1,22 @@
 const filas = document.querySelector(".row") ;
 const columna = document.querySelector(".colum");
 document.querySelector(".btnCrear").addEventListener("click",() =>{
-    alert(`fila ${filas.value} , columna ${columna.value}`)
+    try{
+        if(filas.value <= 0 || columna.value <= 0) throw "datos incorrectos, recuerda que deben ser mayor a 0";
+        else {
+            for (let i = 1 ; i <= filas.value ; i++){
+                let crearFilas = document.createElement("TR");
+                crearFilas.classList.add(`fila${i}`)
+                document.querySelector(".tabla").appendChild(crearFilas);
+                for(let j = 1 ; j <= columna.value ; j++ ){
+                    let crearColumnas = document.createElement("TD");
+                    crearColumnas.classList.add(`columna${j}`)
+                    crearColumnas.innerHTML=`<input class="input${j}" type="text">`
+                    document.querySelector(`.fila${i}`).appendChild(crearColumnas)
+                }
+            }
+        }
+    }catch(e){
+        alert(e)
+    }
 })
