994923fbbd1899b09c3473f8c6dd4f0100041ade
Your NameYour <kevin.guevara090@gmail.com>
2022-10-06 22:00:01 -0500

    se modifico el codigo js y ahora es funcional

--- filasColumnas/code.js
+++ filasColumnas/code.js
@@ -1,47 +1,59 @@
+const containerTransferir = document.querySelector(".containerTransferir")
 const filas = document.querySelector(".row") ;
 const columna = document.querySelector(".colum");
+let int = 0;
+let transferir;
+
 document.querySelector(".btnCrear").addEventListener("click",() =>{
     try{
         if(filas.value <= 0 || columna.value <= 0) throw "datos incorrectos, recuerda que deben ser mayor a 0";
         else {
+            const contenedorRest = document.querySelector(".containerRest");
+            contenedorRest.innerHTML=""
+            containerTransferir.innerHTML=""
             for (let i = 1 ; i <= filas.value ; i++){
-                let crearFilas = document.createElement("TR");
-                crearFilas.classList.add(`fila${i}`)
-                document.querySelector(".tabla").appendChild(crearFilas);
+                let divFilas = `<div class="fila${i}"></div>`;
+                contenedorRest.insertAdjacentHTML("beforeEnd",divFilas);
                 for(let j = 1 ; j <= columna.value ; j++ ){
-                    let crearColumnas = document.createElement("TD");
-                    crearColumnas.classList.add(`columna${j}`)
-                    crearColumnas.innerHTML=`<input class="input${j}" type="text">`
-                    document.querySelector(`.fila${i}`).appendChild(crearColumnas)
+                    let crearColumnas = `<input class="columna${j} type="text""></input>`;
+                    document.querySelector(`.fila${i}`).insertAdjacentHTML("beforeEnd",crearColumnas)
                 }
             }
-            let boton = `<input class="btnCrear btnTransferir" type="button" value="transferir">`;
-            document.querySelector(".tabla").insertAdjacentHTML("afterend",boton)
+            if(int == 0){
+                let boton = `</br><input class="btnCrear btnTransferir" type="button" value="transferir">`;
+                document.querySelector(".containerRest").insertAdjacentHTML("afterend",boton)
+                int = 1
+            }
+            transferir = false;
             cargarDatos()
         }
     }catch(e){
         alert(e)
     }
 })
-
 function cargarDatos () {
     document.querySelector(".btnTransferir").addEventListener("click",() =>{
-        let tabla =`<table class="segundaTabla"></table>`;
-        document.querySelector(".btnTransferir").insertAdjacentHTML("afterend",tabla)
-        let cantFilas = document.querySelectorAll("tr");
-        let cantColum ;
-        cantFilas.forEach(row => {
-            cantColum = (row.childNodes).length;
-        });
-        for (let j = 0 ; j < cantColum ; j++){
-            let crearFilas = document.createElement("TR");
-            crearFilas.classList.add(`row${j+1}`);
-            document.querySelector(`.segundaTabla`).appendChild(crearFilas)
-            const columas = document.querySelectorAll(`.columna${j+1}`);
-            columas.forEach((colum)=>{
-                document.querySelector(`.row${j+1}`).appendChild(colum)
-            })
+        if (transferir != true){
+            containerTransferir.innerHTML=""
+            let cantFilas = document.querySelectorAll(".containerRest > div");
+            let cantColum ;
+            cantFilas.forEach(row => {
+                cantColum = (row.childNodes).length;
+            });
+            for (let j = 0 ; j < cantColum ; j++){
+                let crearFilas = document.createElement("DIV");
+                crearFilas.classList.add(`row${j+1}`);
+                containerTransferir.appendChild(crearFilas)
+                const columas = document.querySelectorAll(`.columna${j+1}`);
+                columas.forEach((colum)=>{
+                    let crearInput = `<input value="${colum.value}">`
+                    document.querySelector(`.row${j+1}`).insertAdjacentHTML("beforeend",crearInput)
+                    console.log(colum.value)
+                })
+            }
+            transferir = true;
         }
     })
 }
+
 
